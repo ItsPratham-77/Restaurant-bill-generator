ecc74c4a28e7528376f131f70a486d209711f975
Pratham prakash Salunkhe <psalunkhe8055@gmail.com>
2025-07-18 23:01:51 +0530

    Add files via upload
    1

---
+++ modelclass.java
@@ -0,0 +1,43 @@
+package model;
+
+public class modelclass {
+    private String dishName;
+    private int dishQuantity;
+    private int dishPrice;
+    private int Total;
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public void setDishName(String dishName) {
+        this.dishName = dishName;
+    }
+
+    public int getDishQuantity() {
+        return dishQuantity;
+    }
+
+    public void setDishQuantity(int dishQuantity) {
+        this.dishQuantity = dishQuantity;
+    }
+
+    public int getDishPrice() {
+        return dishPrice;
+    }
+
+    public void setDishPrice(int dishPrice) {
+        this.dishPrice = dishPrice;
+    }
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int total) {
+        Total = total;
+    }
+
+
+
+}
